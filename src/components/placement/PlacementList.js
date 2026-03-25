1b50c19ee3751d6417dcc2e8abecb134722ec0ee
Brennan Davenport <brennandavenport9@gmail.com>
2026-03-25 12:06:51 -0400

    fixed mobile view for placements

--- src/components/placement/PlacementList.js
+++ src/components/placement/PlacementList.js
@@ -5,11 +5,9 @@ import Link from "next/link";
 export default function PlacementList({ limit }) {
   const placement = limit ? macros.placements.slice(0, limit) : macros.placements;
 
-  // this is broken on mobile view :(
   return (
-    <div className="grid gap-6" style={{ gridTemplateColumns: "repeat(3, 300px)" }}>
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {placement.map((_, index) => {
-        const place = macros.placements[index];
         return (
             <Placement key={index} index={index} />
         );
