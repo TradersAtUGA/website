f272660b5babb53af627f8e30a85cb7c2bc2aebc
Brennan Davenport <brennandavenport9@gmail.com>
2026-09-08 21:13:35 -0400

    updated form

--- src/macros.js
+++ src/macros.js
@@ -43,10 +43,10 @@ export const macros = {
     "Traders@UGA is the University of Georgia's premier Quantitative Finance Club, dedicated to fostering innovation, education, and collaboration in trading, quantitative research, and market analysis. Our mission is to bridge the gap between academia and the financial industry, empowering students to transform their theoretical knowledge into practical applications.",
   join_text:
     "Are you passionate about trading, quantitative research, or applying software engineering skills in the world of finance? Traders@UGA is a project-based community for ambitious individuals who want to make an impact by working on real-world, high-level financial challenges.",
-  join_disabled: true,
+  join_disabled: false,
   application_opens: "Fall 2026",
   join_link:
-    "https://forms.gle/x2degEXZveXUZRrx9",
+    "https://forms.gle/U19kWaaCXT8kEMav7",
   contact_text:
     "We'd love to hear from you! Reach out to us at any of the following:",
   members: [
